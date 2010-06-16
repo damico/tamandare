063432128387ec1e5ab878022b2a28ab9538dc08
jd.comment <jd.comment@a4ed73ac-f325-11dd-802b-177316e4ac06>
2010-06-16 22:18:07 +0000

    Added basic scheduler features

--- tamandare/src/org/jdamico/tamandare/threads/StartSchedulerMonitorThread.java
+++ tamandare/src/org/jdamico/tamandare/threads/StartSchedulerMonitorThread.java
@@ -1,5 +1,13 @@
 package org.jdamico.tamandare.threads;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.jdamico.tamandare.components.LoggerManager;
+import org.jdamico.tamandare.dataobjects.Job;
+import org.jdamico.tamandare.transactions.TransactionManager;
+import org.jdamico.tamandare.utils.TamandareHelper;
+
 
 public class StartSchedulerMonitorThread extends Thread {
 
@@ -12,8 +20,26 @@ public class StartSchedulerMonitorThread extends Thread {
 		
 		try {
 			while (true) {
-				ThreadRunnableManager.getInstance().privateNetSyncThread();
-				System.out.println("go!!!!!!!!!!!!");
+				
+				TransactionManager tm = new TransactionManager();
+				ArrayList<Job> jobs = tm.getJobs();
+				if(jobs!=null && jobs.size() > 0){
+					for(int i=0; i<jobs.size(); i++){
+						if(TamandareHelper.getInstance().isReady(new Date(), jobs.get(i).getTimestamp(), jobs.get(i).getInterval())){
+							System.out.println("Go baby!");
+							if(jobs.get(i).getThread().equals("privateNetSyncThread")){
+								ThreadRunnableManager.getInstance().privateNetSyncThread();
+								tm.updateKeepSync(new Date());
+							}else if(jobs.get(i).getThread().equals("killJob")){
+								tm.deleteJobByName("killJob");
+								LoggerManager.getInstance().logAtDebugTime(this.getClass().getName(), "Killing me softly...");
+								System.exit(0);
+							} 
+						}
+					}
+				}
+				
+				System.out.println("Monitor loop!");
 				Thread.sleep(30000);
 			}
 			
