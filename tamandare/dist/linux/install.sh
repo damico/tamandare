ede46db8f9cb8dc2559650334fabe1ac04ec281b
jd.comment <jd.comment@a4ed73ac-f325-11dd-802b-177316e4ac06>
2010-06-16 22:23:17 +0000

    Fixed install script, using tee command

--- tamandare/dist/linux/install.sh
+++ tamandare/dist/linux/install.sh
@@ -6,11 +6,11 @@ sudo cp tamandare.xsd /etc/tamandare/tamandare.xsd
 sudo mkdir /var/log/tamandare
 sudo touch /var/log/tamandare/tamandare.log
 sudo mkdir /opt/derby
-sudo echo > "dbpath = /home/`$(whoami)`/.tamandare/db"
 sudo chmod 755 /etc/init.d/tamandare
 sudo chmod 755 startTamandareAgents.sh
+echo "dbpath = /home/$(whoami)/.tamandare/db" | sudo tee -a /etc/tamandare/tamandare.conf
+echo "whoami = $(whoami)-$(hostname)" | sudo tee -a /etc/tamandare/tamandare.conf
 sudo update-rc.d tamandare defaults
-
 echo ""
 echo "BEFORE RUN TAMANDARE YOU MUST EDIT /etc/tamandare/tamandare.conf" 
 echo ""
